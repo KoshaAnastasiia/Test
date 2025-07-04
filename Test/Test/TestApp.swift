4de99f990582f28c08e64a93041a20389f0b7813
kosha <koshaanastasiia@gmail.com>
2025-07-04 12:35:55 +0200

    start

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by kosha on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
